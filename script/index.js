c585ade412b46ac5caf49d6c92ebe4ef2c48294b
Tushar Tiwari <tushartiwari0172@gmail.com>
2022-01-29 21:58:58 +0530

    bug: links Sidebar to the DOM

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 const headerEl = document.getElementById("header");
 const navigationEl = document.getElementById("navigation");
+const sidebarEl = document.getElementById("sidebar");
 
 // load header component
 fetch("/layout/header/header.html")
@@ -11,4 +12,10 @@ fetch("/layout/header/header.html")
 fetch("/layout/navigation/navigation.html")
 .then(res => res.text())
 .then(html => navigationEl.innerHTML = html)
+.catch(err => console.log(err));
+
+// load sidebar Component
+fetch("/layout/sidebar/sidebar.html")
+.then(res => res.text())
+.then(html => sidebarEl.innerHTML = html)
 .catch(err => console.log(err));
