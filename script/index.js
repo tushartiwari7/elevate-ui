a3f4032cb3e18be17612400b1db40bc8aef81f7d
Tushar Tiwari <tushartiwari0172@gmail.com>
2022-01-29 16:50:10 +0530

    refactor: divided components used by App and those consumed by library for outsourcing

--- script/index.js
+++ script/index.js
@@ -2,13 +2,13 @@ const headerEl = document.getElementById("header");
 const navigationEl = document.getElementById("navigation");
 
 // load header component
-fetch("/components/header/index.html")
+fetch("/layout/header/header.html")
 .then(res => res.text())
 .then(html => headerEl.innerHTML = html)
 .catch(err => console.log(err));
 
 // load navbar component
-fetch("/components/navigation/index.html")
+fetch("/layout/navigation/navigation.html")
 .then(res => res.text())
 .then(html => navigationEl.innerHTML = html)
 .catch(err => console.log(err));
