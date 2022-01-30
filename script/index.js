10a97fa19d39988bef988ffa52731a9dba3bc55b
Tushar Tiwari <tushartiwari0172@gmail.com>
2022-01-31 01:13:44 +0530

    feat: default alert variant

--- script/index.js
+++ script/index.js
@@ -2,10 +2,9 @@ const headerEl = document.getElementById("header");
 const headerTitle = document.getElementById("header-title");
 const navigationEl = document.getElementById("navigation");
 let previewEl = document.getElementById("preview");
-let componentDesc = document.getElementById("desc");
+let descEl = document.getElementById("desc");
 let codeEl = document.getElementById("code");
 let footerEl = document.getElementById("footer");
-let BadgeBtn = document.getElementById("BadgeBtn");
 
 const clickedItems = [];
 const removeActiveAll = () => {
@@ -27,7 +26,7 @@ const loadAvatar = (src) => {
   //description
   fetch("./components/avatar/description.html")
   .then(res => res.text())
-  .then(html => componentDesc.innerHTML = html)
+  .then(html => descEl.innerHTML = html)
   .catch(err => console.log(err));
 
   //navigation
@@ -91,6 +90,48 @@ const loadTextAvatar = (src) => {
   .then(res => res.text())
   .then(html => codeEl.innerHTML = html)
   .catch(err => console.log(err));
+}
+
+//load alert component
+
+const loadAlert = (src) => {
+  // make alert nav-btn active
+  removeActiveAll();
+  clickedItems.push(src);
+  src?.classList.add("active");
+
+  // header title
+  document.getElementById('header-title').innerText = "Alert Component";
+  //description
+  fetch("./components/alert/description.html")
+  .then(res => res.text())
+  .then(html => descEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //navigation
+  fetch("/components/alert/nav.html")
+  .then(res => res.text())
+  .then(html => navigationEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  // preview
+  fetch("./components/alert/preview.html")
+  .then(res => res.text())
+  .then(html => previewEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  // //code
+  fetch("./components/alert/code.html")
+  .then(res => res.text())
+  .then(html => codeEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //footer
+  fetch("./components/alert/footer.html")
+  .then(res => res.text())
+  .then(html => footerEl.innerHTML = html)
+  .catch(err => console.log(err));
+
 }
 
 loadAvatar(null);
