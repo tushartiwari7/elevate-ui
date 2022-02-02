eed7a738646d4b9c038ef8e87ded35a39e2d997c
Tushar Tiwari <tushartiwari0172@gmail.com>
2022-02-03 00:58:13 +0530

    feat: card layout: text overlay

--- script/index.js
+++ script/index.js
@@ -377,4 +377,49 @@ const loadRoundedButton = src => {
   .catch(err => console.log(err));
 }
 
+
+// ------------------------------------------Card Component--------------------------------------------
+
+
+const loadCard = src => {
+  // make card nav-btn active
+  removeActiveAll();
+  clickedItems.push(src);
+  src?.classList.add("active");
+
+  // header title
+  headerTitle.innerText = "Card Component";
+  
+  //description
+  fetch("./components/card/description.html")
+  .then(res => res.text())
+  .then(html => descEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //navigation
+  fetch("/components/card/nav.html")
+  .then(res => res.text())
+  .then(html => navigationEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  // preview
+  fetch("./components/card/default/preview.html")
+  .then(res => res.text())
+  .then(html => previewEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //code
+  fetch("./components/card/default/code.html")
+  .then(res => res.text())
+  .then(html => codeEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //footer
+  fetch("./components/card/footer.html")
+  .then(res => res.text())
+  .then(html => footerEl.innerHTML = html)
+  .catch(err => console.log(err));
+}
+
+
 loadAvatar(null);
