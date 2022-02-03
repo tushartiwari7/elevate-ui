dca25ea4c1f34f306806efe6591af8aaac59dd46
Tushar Tiwari <tushartiwari0172@gmail.com>
2022-02-03 15:45:49 +0530

    feat: Images component

--- script/index.js
+++ script/index.js
@@ -460,7 +460,6 @@ const dismissableCard = src => {
   .catch(err => console.log(err));
 }
 
-
 const textOnlyCard = src => {
 
   // make card nav-btn active
@@ -501,5 +500,87 @@ const horizontalCard = src => {
   .catch(err => console.log(err));
   
 }
+
+// ------------------------------------------Image Component--------------------------------------------
+
+const loadImage = src => {
+  // make image nav-btn active
+  removeActiveAll();
+  clickedItems.push(src);
+  src?.classList.add("active");
+
+  // header title
+  headerTitle.innerText = "image Component";
+  
+  //description
+  fetch("./components/image/description.html")
+  .then(res => res.text())
+  .then(html => descEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //navigation
+  fetch("/components/image/nav.html")
+  .then(res => res.text())
+  .then(html => navigationEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  // preview
+  fetch("./components/image/square/preview.html")
+  .then(res => res.text())
+  .then(html => previewEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //code
+  fetch("./components/image/square/code.html")
+  .then(res => res.text())
+  .then(html => codeEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //footer
+  fetch("./components/image/footer.html")
+  .then(res => res.text())
+  .then(html => footerEl.innerHTML = html)
+  .catch(err => console.log(err));
+}
+
+const loadRoundedImage = src => {
+  // make image nav-btn active
+  removeActiveAll();
+  clickedItems.push(src);
+  src?.classList.add("active");
+
+  // preview
+  fetch("./components/image/rounded/preview.html")
+  .then(res => res.text())
+  .then(html => previewEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //code
+  fetch("./components/image/rounded/code.html")
+  .then(res => res.text())
+  .then(html => codeEl.innerHTML = html)
+  .catch(err => console.log(err));
+}
+
+const loadRespImage = src => {
+  // make image nav-btn active
+  removeActiveAll();
+  clickedItems.push(src);
+  src?.classList.add("active");
+
+  // preview
+  fetch("./components/image/responsive/preview.html")
+  .then(res => res.text())
+  .then(html => previewEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //code
+  fetch("./components/image/responsive/code.html")
+  .then(res => res.text())
+  .then(html => codeEl.innerHTML = html)
+  .catch(err => console.log(err));
+}
+
+
 
 loadAvatar(null);
