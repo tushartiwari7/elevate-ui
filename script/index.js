9340c451ce36da233d1bd81a0fa6e72936a78972
Tushar Tiwari <tushartiwari0172@gmail.com>
2022-02-03 22:14:50 +0530

    feat: input component

--- script/index.js
+++ script/index.js
@@ -581,6 +581,87 @@ const loadRespImage = src => {
   .catch(err => console.log(err));
 }
 
+// ------------------------------------------Input Component--------------------------------------------
+
+const loadInput = src => {
+  // make input nav-btn active
+  removeActiveAll();
+  clickedItems.push(src);
+  src?.classList.add("active");
+
+  // header title
+  headerTitle.innerText = "Input Component";
+  
+  //description
+  fetch("./components/input/description.html")
+  .then(res => res.text())
+  .then(html => descEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //navigation
+  fetch("/components/input/nav.html")
+  .then(res => res.text())
+  .then(html => navigationEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  // preview
+  fetch("./components/input/default/preview.html")
+  .then(res => res.text())
+  .then(html => previewEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //code
+  fetch("./components/input/default/code.html")
+  .then(res => res.text())
+  .then(html => codeEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //footer
+  fetch("./components/input/footer.html")
+  .then(res => res.text())
+  .then(html => footerEl.innerHTML = html)
+  .catch(err => console.log(err));
+}
+
+const loadTextBoxInput = src => {
+  // make input nav-btn active
+  removeActiveAll();
+  clickedItems.push(src);
+  src?.classList.add("active");
+
+  // preview
+  fetch("./components/input/textbox/preview.html")
+  .then(res => res.text())
+  .then(html => previewEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //code
+  fetch("./components/input/textbox/code.html")
+  .then(res => res.text())
+  .then(html => codeEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+}
+
+const loadValidationInput = src => {
+  // make input nav-btn active
+  removeActiveAll();
+  clickedItems.push(src);
+  src?.classList.add("active");
+
+  // preview
+  fetch("./components/input/validation/preview.html")
+  .then(res => res.text())
+  .then(html => previewEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+  //code
+  fetch("./components/input/validation/code.html")
+  .then(res => res.text())
+  .then(html => codeEl.innerHTML = html)
+  .catch(err => console.log(err));
+
+}
 
 
 loadAvatar(null);
